2885a720afd835c3068a7d544161501979619d37
Chinni-12h <tulasiposimsettiap9@gmail.com>
2024-12-03 12:49:29 +0530

    feature202 changes by Pavan

--- application.java
+++ application.java
@@ -18,4 +18,16 @@ class Main {
     int result = obj.addNumbers(num1, num2);
     System.out.println("Sum is: " + result);
   }
+  #feature202 changes by pavan for the report module in oct2023
+  public int addNumbers(int a, int b) {
+    int sum = a + b;
+    return sum;
+}
+
+public static void main(String[] args) {
+    Main obj = new Main();
+    int result = obj.addNumbers(25, 15);
+    System.out.println("Sum is: " + result);
+}
+#end of feature202 code
 }
